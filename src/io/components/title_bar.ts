2df2d3c7960d23e7f79c4c9ecca0ad085872989a
augvic <a.viceli7357@gmail.com>
2025-10-19 21:02:50 -0300

    Update in ascii elements.

--- src/io/components/title_bar.ts
+++ src/io/components/title_bar.ts
@@ -61,7 +61,7 @@ class MaximizeButton {
     private createSelf() {
         this.element = document.createElement("button");
         this.element.className = "bg-gray-950 text-white h-full w-10 hover:bg-gray-700 app-region-no-drag cursor-default transition-colors duration-300";
-        this.element.innerText = "🗖";
+        this.element.innerText = "□";
     }
     
     private startListeners() {
