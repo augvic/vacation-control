f3a6d6380a48a90515c3c1c50173e25fc5488d5b
augvic <a.viceli7357@gmail.com>
2025-11-01 21:18:51 -0300

    Created database component.

---
+++ electron/src/components/database.ts
@@ -0,0 +1,35 @@
+import Database from "better-sqlite3";
+import type { Database as DatabaseType } from "better-sqlite3";
+import { join } from "path";
+import { app } from "electron";
+
+export class DbHandler {
+    
+    db: DatabaseType
+    
+    constructor() {
+        let dbPath = "";
+        if (app.isPackaged) {
+            dbPath = join(process.resourcesPath, "database.db");
+        } else {
+            dbPath = join(__dirname, "..", "..", "storage", "database.db");
+        }
+        this.db = new Database(dbPath);
+        this.db.prepare(`
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user TEXT NOT NULL,
+                admission TEXT NOT NULL
+            )
+        `).run();
+    }
+    
+    create(user: string, admission: string) {
+        this.db.prepare("INSERT INTO users (name, admission) VALUES (?, ?)").run(user, admission);
+    }
+    
+    read(): [{ id: number; user: string; admission: string }] {
+        return this.db.prepare("SELECT * FROM users").all() as [{ id: number; user: string; admission: string }];
+    }
+    
+}
